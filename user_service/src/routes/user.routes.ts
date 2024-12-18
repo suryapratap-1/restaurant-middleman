9a11e854b06517e3f6af8dfae5dc1e2d5665f05b
suryapratap-1 <suryapratap0765@gmail.com>
2024-12-18 17:23:54 +0530

    Add update user route for type admin and super admin

--- user_service/src/routes/user.routes.ts
+++ user_service/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/user.controller";
+import { registerUser, loginUser, logoutUser, updateUser } from "../controllers/user.controller";
 import { authenticateToken } from "../middleware/middleware";
 
 export const authRouter = Router();
@@ -7,3 +7,4 @@ export const authRouter = Router();
 authRouter.route("/register").post(registerUser)
 authRouter.route("/login").post(loginUser)
 authRouter.route("/logout").get(authenticateToken, logoutUser)
+authRouter.route("/profile/:id").patch(authenticateToken, updateUser)
