2ca88936c50ecc6530994591daa661399f351fe8
suryapratap-1 <suryapratap0765@gmail.com>
2024-12-18 17:22:09 +0530

    Add user service to create store and admins

---
+++ user_service/src/service/user.services.ts
@@ -0,0 +1,118 @@
+import { Types } from 'mongoose';
+import User from '../models/user.model';
+import Store, { IStoreDocument} from '../models/store.model';
+import Address from '../models/address.model';
+import { hashPassword } from '../utils/argon2.utils';
+import { validateAddressData, validateStoreData, validateUserData } from '../utils/validator';
+
+interface IUser {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phone: string;
+    profilePicture?: string;
+    role: 'super_admin' | 'admin' | 'manager' | 'staff';
+    restaurantId: Types.ObjectId;
+}
+
+interface IStore {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    phone: string;
+    profilePicture?: string;
+    role: 'super_admin' | 'admin' | 'manager' | 'staff';
+    storeDetails: {
+        storeName: string;
+        notificationPhone: string;
+        openTime: string;
+        closeTime: string;
+        holidayList?: Date[];
+        partnerWith: string;
+        partnerReferenceId?: Types.ObjectId;
+        deliveryRange?: number;
+        storeStatus?: 'active' | 'busy';
+        translation?: string[];
+    };
+    addressDetails: {
+        location: string;
+        city: string;
+        state: string;
+        country: string;
+        postal_code: string;
+        geo_longitude: number;
+        geo_latitude: number;
+    };
+}
+
+export const createAdminOrSuperAdmin = async (userData: IUser): Promise<IUser> => {
+    const { firstName, lastName, email, password, phone, profilePicture, role } = userData;
+    
+    validateUserData({ firstName, lastName, email, password, phone, role });
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        phone,
+        profilePicture,
+        role,
+        restaurantId: null,  // Admins don't have stores
+    });
+
+    await user.save();
+    return user;
+};
+
+export const createManagerOrStaff = async (storeData: IStore): Promise<IStoreDocument> => {
+    const { firstName, lastName, email, password, phone, profilePicture, role, storeDetails, addressDetails } = storeData;
+
+    validateUserData({ firstName, lastName, email, password, phone, role });
+    validateAddressData(addressDetails);
+    validateStoreData(storeDetails);
+
+    const hashedPassword = await hashPassword(password);
+
+    // Create the Address
+    const address = new Address(addressDetails);
+    await address.save();
+
+    // Create the Store
+    const store = new Store({
+        storeName: storeDetails.storeName,
+        userDetails: null,
+        address: address._id,
+        notificationPhone: storeDetails.notificationPhone,
+        openTime: storeDetails.openTime,
+        closeTime: storeDetails.closeTime,
+        holidayList: storeDetails.holidayList || [],
+        partnerWith: storeDetails.partnerWith,
+        partnerReferenceId: storeDetails.partnerReferenceId,
+        deliveryRange: storeDetails.deliveryRange || 0,
+        storeStatus: storeDetails.storeStatus || 'active',
+        translation: storeDetails.translation || [],
+    });
+
+    // Create the User
+    const user = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        phone,
+        profilePicture,
+        role,
+        restaurantId: store._id
+    });
+
+    store.userDetails = user._id as Types.ObjectId;
+    await store.save();
+    await user.save();
+
+    return store;
+};
