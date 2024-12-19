b87088673f3b209b4db2983845edb0ab7e7678d4
suryapratap-1 <suryapratap0765@gmail.com>
2024-12-19 17:25:42 +0530

    ~Bug fix for invalid token

--- user_service/src/middleware/middleware.ts
+++ user_service/src/middleware/middleware.ts
@@ -3,6 +3,13 @@ import { config } from "../config/variables.config";
 import { verifyToken } from "../utils/auth.utils";
 
 export const authenticateToken = (req: any, res: any, next: any) => {
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Authorization header is required.' });
+    }
+
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
         return res.status(401).json({ message: "No token provided." });
@@ -19,14 +26,22 @@ export const authenticateToken = (req: any, res: any, next: any) => {
 
 export const authorizeStore = (req: any, res: any, next: any) => {
 
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+        return res.status(401).json({ error: 'Authorization header is required.' });
+    }
+
     const token = req.headers.authorization.split(" ")[1];
     if (!token) return res.status(401).json({ message: "No token provided." });
 
     try {
         const isValid = verifyToken(token);
-        if (isValid) next();
-    } 
-    catch (error) {
-        return res.status(403).json({ message: "Invalid token." });
+        console.log("isValid", isValid);
+        if (!isValid) throw new Error("Token is invalid.");
+        next();
+    }
+    catch (error: any) {
+        return res.status(403).json({ message: error.message });
     }
 };
