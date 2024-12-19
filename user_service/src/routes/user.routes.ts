43690b604316769944b6b280c99f9539f4328208
suryapratap-1 <suryapratap0765@gmail.com>
2024-12-19 15:50:20 +0530

    Add patch route to update store

--- user_service/src/routes/user.routes.ts
+++ user_service/src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, updateUser, generateStoreToken } from "../controllers/user.controller";
-import { authenticateToken } from "../middleware/middleware";
+import { registerUser, loginUser, logoutUser, updateUser, generateStoreToken, updateStore } from "../controllers/user.controller";
+import { authenticateToken, authorizeStore } from "../middleware/middleware";
 
 export const authRouter = Router();
 
@@ -11,3 +11,4 @@ authRouter.route("/profile/:id").patch(authenticateToken, updateUser)
 
 // store
 authRouter.route("/store/generate-token").post(generateStoreToken)
+authRouter.route("/store/:id").patch(authorizeStore, updateStore)
