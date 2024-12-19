577069fd3aa50b599eea18ea7d4463b97f2d2ff4
suryapratap-1 <suryapratap0765@gmail.com>
2024-12-19 13:10:28 +0530

    Update password utility functions

---
+++ user_service/src/utils/password.utils.ts
@@ -0,0 +1,19 @@
+import argon2 from 'argon2';
+
+const hashPassword = async (password: string) => {
+    try {
+        const hashedPassword = await argon2.hash(password);
+        return hashedPassword;
+    } catch (error) {
+        throw new Error('Password hashing failed');
+    }
+};
+
+const verifyPassword = async (storedPassword: string, password: string): Promise<void> => {
+    const isMatch = await argon2.verify(storedPassword, password);
+
+    if (!isMatch) throw new Error('Invalid password');
+    else throw new Error('Password verification failed');
+};
+
+export { hashPassword, verifyPassword };
