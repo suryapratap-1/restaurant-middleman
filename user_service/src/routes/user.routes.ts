dad17a550e5d051c0cd5b92c7689940540b5ae97
suryapratap-1 <suryapratap0765@gmail.com>
2024-12-19 16:15:43 +0530

    Update route for admin update controller

--- user_service/src/routes/user.routes.ts
+++ user_service/src/routes/user.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, updateUser, generateStoreToken, updateStore } from "../controllers/user.controller";
+import { registerUser, loginUser, logoutUser, updateAdminOrSuperAdmin, generateStoreToken, updateStore } from "../controllers/user.controller";
 import { authenticateToken, authorizeStore } from "../middleware/middleware";
 
 export const authRouter = Router();
@@ -7,7 +7,7 @@ export const authRouter = Router();
 authRouter.route("/register").post(registerUser)
 authRouter.route("/login").post(loginUser)
 authRouter.route("/logout").get(authenticateToken, logoutUser)
-authRouter.route("/profile/:id").patch(authenticateToken, updateUser)
+authRouter.route("/profile/:id").patch(authenticateToken, updateAdminOrSuperAdmin)
 
 // store
 authRouter.route("/store/generate-token").post(generateStoreToken)
